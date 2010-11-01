6b29ed437f2ee9e29e17908a8bca7c792c961599
dan <dan@test2.tofubar>
2010-11-02 09:43:34 +1100

    Fix up interface view. Clean up soe of the back linking.

--- cmd.php
+++ cmd.php
@@ -3,6 +3,14 @@ function footer(){
 echo '<a href="cmd.php?Action=showallhosts"> show all hosts </a>';
 }
 
+function showhostname($hostid){
+$query = "select name from host where id=".$hostid.";";
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+$line = pg_fetch_array($result,0,PGSQL_ASSOC);
+echo $line['name'];
+return $line['name'];
+}
+
 function showallhosts(){
 	$query = 'SELECT * FROM host';
 	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
@@ -23,13 +31,13 @@ function showallhosts(){
 }
 
 function showhost($hostid){
-	$query = "select  host.name as linkhostname, host.id as linkhostid, q4.name as linkinterface, q4.intname from host right outer join\n";
+	$query = "select  host.name as linkhostname, host.id as linkhostid, q4.name as linkinterface, q4.intname, q4.intid as intid from host right outer join\n";
 	$query .=		"(select interface.id as i2id, interface.name, interface.host as i2host, q3.* from interface right outer join\n";
-	$query .=			"(select l2.id as l2bid, l2.link as l2blink, l2.interface as l2bint, q2.* from l2 right outer join\n";
-	$query .=			"	(select l2.id as l2aid, l2.link as l2alink, l2.interface as l2aint, q1.intid, q1.intname from l2 right outer join\n";
+	$query .=			"(select physicallink.id as l2bid, physicallink.link as l2blink, physicallink.interface as l2bint, q2.* from physicallink right outer join\n";
+	$query .=			"	(select physicallink.id as l2aid, physicallink.link as l2alink, physicallink.interface as l2aint, q1.intid, q1.intname from physicallink right outer join\n";
 	$query .=			"		(select interface.id as intid ,interface.name as intname from interface where interface.host=".$hostid.") as q1\n";
-	$query .=			"	on l2.interface = q1.intid) as q2\n";
-	$query .=		"	on l2.link = q2.l2alink and l2.id != q2.l2aid) as q3\n";
+	$query .=			"	on physicallink.interface = q1.intid) as q2\n";
+	$query .=		"	on physicallink.link = q2.l2alink and physicallink.id != q2.l2aid) as q3\n";
 	$query .=		"on interface.id = q3.l2bint) as q4\n";
 	$query .=	"on host.id = q4.i2host;\n";
 #	echo $query."\n";
@@ -38,7 +46,7 @@ function showhost($hostid){
 	echo "<tr><td>interface</td><td>linked host</td><td>linked interface</td></tr>\n";
 	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
 		echo "\t<tr>\n";
-		echo "<td><a href=cmd.php?id=".$line['id'].">".$line['intname']."</a></td><td><a href=cmd.php?action=showhost&hostid=".$line['linkhostid'].">".$line['linkhostname']."</a></td><td>".$line['linkinterface']."</td>\n";
+		echo "<td><a href=cmd.php?action=showint&intid=".$line['intid']."&hostid=".$hostid.">".$line['intname']."</a></td><td><a href=cmd.php?action=showhost&hostid=".$line['linkhostid'].">".$line['linkhostname']."</a></td><td>".$line['linkinterface']."</td>\n";
 		echo "\t</tr>\n";
 	}
 	echo "</table>";
@@ -62,6 +70,7 @@ if ( $_GET['action'] == 'showallhosts' ) {
         $result = pg_query($query) or die('Query failed: ' . pg_last_error());
         pg_free_result($result);
 	showallhosts();
+	echo "<a href=cmd.php?action=showallhosts> show all hosts </a>";
 } else if  ( $_GET['action'] == 'addhost' ) {
 	echo "about to add host\n";
         $query = "INSERT INTO host VALUES (DEFAULT,"."'".$_GET['hostname']."'".") returning *";
@@ -69,7 +78,11 @@ if ( $_GET['action'] == 'showallhosts' ) {
         pg_free_result($result);
 	showallhosts();
 } else if ( $_GET['action'] == 'showhost' ) {
+	echo "<h1>\n";
+	showhostname($_GET['hostid']);
+	echo "</h1>\n";
 	showhost($_GET['hostid']);
+	echo "<a href=cmd.php?action=showallhosts>showallhosts</a>";
 } else if ( $_GET['action'] == 'addinterface' ) {
 	echo "about to add interface\n";
         $query = "INSERT INTO interface VALUES (DEFAULT,"."'".$_GET['intname']."',".$_GET['hostid'].") returning *";
@@ -77,13 +90,13 @@ if ( $_GET['action'] == 'showallhosts' ) {
         pg_free_result($result);
 	showhost($_GET['hostid']);
 
-} else if ( ( $_GET['action'] == 'addl2link' ) && isset($_GET['h1'],$_GET['h2']) ) {
+} else if ( ( $_GET['action'] == 'addlink' ) && isset($_GET['h1'],$_GET['h2']) ) {
 	echo 'pick interfaces';
 	$query = "select * from interface where host=".$_GET['h1'].";";
         $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 	echo '<form name="new link" action="cmd.php" method="get">';
         echo ' <input type="submit" value="Submit" />';
-        echo ' <input type="hidden" name="action" value="addl2link" />';
+        echo ' <input type="hidden" name="action" value="addlink" />';
 	echo ' <select name="i1"> ';
 	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
 		echo ' <option value="'.$line['id'].'"> '.$line['name'].' </option>';
@@ -98,22 +111,22 @@ if ( $_GET['action'] == 'showallhosts' ) {
 	echo ' </select> ';
 	echo ' </form> ';
 	
-} else if ( ( $_GET['action'] == 'addl2link' ) && isset($_GET['i1'],$_GET['i2']) ) {
+} else if ( ( $_GET['action'] == 'addlink' ) && isset($_GET['i1'],$_GET['i2']) ) {
 	$query = "insert into link values ( default, '".$_GET['link_name']."') returning id;";
 	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 	$line = pg_fetch_array($result,null, PGSQL_ASSOC);
 
-	$query = "insert into l2 values ( default, '".$line['id']."' , '".$_GET['i1']."' ) ;  insert into l2 values ( default, '".$line['id']."' , '".$_GET['i2']."' ) ; ";
+	$query = "insert into physicallink values ( default, '".$line['id']."' , '".$_GET['i1']."' ) ;  insert into physicallink values ( default, '".$line['id']."' , '".$_GET['i2']."' ) ; ";
         $result = pg_query($query) or die('Query failed: ' . pg_last_error());
         $line = pg_fetch_array($result,null, PGSQL_ASSOC);	
 
-} else if ( $_GET['action'] == 'addl2link' ) {
+} else if ( $_GET['action'] == 'addlink' ) {
 	$query = "select * from host;";
         $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 	echo '<form name="new link" action="cmd.php" metho="get">';
 	echo 'New Link Name: <input type="text" name="link_name" />';
         echo ' <input type="submit" value="Submit" />';
-        echo ' <input type="hidden" name="action" value="addl2link" />';
+        echo ' <input type="hidden" name="action" value="addlink" />';
 	echo ' <select name="h1"> ';
 	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
 		echo ' <option value="'.$line['id'].'"> '.$line['name'].' </option>';
@@ -127,5 +140,24 @@ if ( $_GET['action'] == 'showallhosts' ) {
 	}
         echo "</select>";
         echo "</form>";
+} else if ( $_GET['action'] == 'showint' ) {
+	$query = "select * from interface where id=".$_GET['intid'].";";
+        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+		echo  $line['name'];
+	}
+	echo '<form name="changeint" action="cmd.php" method="get">';
+	echo 'interface name: <input type="text" name="intname" /> <br/>';
+	echo '<input type="submit" value="Submit" />';
+	echo '<input type="hidden" name="action" value="changeint" />';
+	echo '<input type="hidden" name="intid" value="'.$_GET['intid'].'" />';
+	echo "</form>";
+	echo "<a href=cmd.php?action=showhost&hostid=".$_GET['hostid']."> back to host </a>";
+} else if ( $_GET['action'] == 'changeint' ) {
+	if (isset($_GET['intid'],$_GET['intname'])) {
+		echo "about to update interface\n";
+		$query = "update interface SET name='".$_GET['intname']."' where id=".$_GET['intid'].";";
+		$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+	}
 }
 ?>
