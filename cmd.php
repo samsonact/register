970a36b49617c37b2e12ac76d25b49d719179619
dan <dan@test2.tofubar>
2010-12-06 20:23:32 +1100

    no idea

--- cmd.php
+++ cmd.php
@@ -106,11 +106,12 @@ if ( $_GET['action'] == 'showallhosts' ) {
         echo "</select>";
         echo "</form>";
 } else if ( $_GET['action'] == 'showint' ) {
-	$query = "select * from interface where id=".$_GET['intid'].";";
+	$query = " select i1.name as iname, b.name as aname, b.vlan,b.subintid as subintid  from interface i1 left join (SELECT s1.id as subintid, * from address a1 right outer join subint s1 on a1.subint = s1.id ) as b on i1.id = b.interface where i1.id=".$_GET['intid'].";";
         $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-		echo  $line['name'].'<a href="cmd.php?action=addsubint&intid='.$line['id'].'"> Add subint </a>';
+		echo  $line['iname']." ".$line['vlan']." <a href=cmd.php?action=changevlan&subint=".$line['subintid'].">change vlan</a>  <a href=cmd.php?action=delsubint&subint=".$line['subintid'].">delete subint</a><br>\n";
 	}
+	echo  '<a href="cmd.php?action=addsubint&intid='.$_GET['intid'].'"> Add subint </a>';
 	echo '<form name="changeint" action="cmd.php" method="get">';
 	echo 'change interface name: <input type="text" name="intname" /> ';
 	echo '<input type="submit" value="Submit" />';
@@ -139,7 +140,6 @@ if ( $_GET['action'] == 'showallhosts' ) {
 		echo "<h1> deleted link </h1>";
 		showhost($_GET['hostid']);
 		echo "<a href=cmd.php?action=showallhosts>showallhosts</a>";
-
 	}
 } else if ( $_GET['action'] == 'addsubint' ) {
 	##XXX do vlan and and intid check
@@ -147,6 +147,28 @@ if ( $_GET['action'] == 'showallhosts' ) {
 	$query = "insert into subint values ( default,".$_GET['intid'].",default );";
 	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 	echo "added subint\n";
-
+} else if ( $_GET['action'] == 'delsubint' ) {
+	echo "about to delete subint\n";
+	$query = "delete from subint where id=".$_GET['subint'].";"; 
+	echo $query;
+	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+	echo "deleted subint\n";
+} else if ( $_GET['action'] == 'changevlan' ) {
+	if (isset($_GET['vlan'],$_GET['subint'])) {
+		$query = "update subint set vlan=".$_GET['vlan']." where  id=".$_GET['subint'].";";
+		$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+	} else {	
+		$query = "select * from vlan;";
+		$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+		echo '<form name="changevlan" action="cmd.php" method="get">';
+		echo '<input type="submit" value="Submit" />';
+		echo '<input type="hidden" name="action" value="changevlan" />';
+		echo '<input type="hidden" name="subint" value="'.$_GET['subint'].'" />';
+		echo ' <select name="vlan"> ';
+		while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+			echo ' <option value="'.$line['id'].'"> '.$line['name'].":".$line['number'].' </option>';
+		}
+		echo "</form>";
+	}
 }
 ?>
